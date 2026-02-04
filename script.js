dcd412465855c9ba227e2c3b0e5bd5530b85e7f8
Cason(Chenxu) Huang <148577178+agedhuang@users.noreply.github.com>
2026-02-04 04:24:33 -0500

    020426

---
+++ script.js
@@ -0,0 +1,37 @@
+var allItems = document.querySelectorAll('.grid-column img, .grid-column .iframe-wrapper, .grid-column .text-block');
+
+allItems.forEach(function (item) {
+  item.addEventListener('click', function () {
+    var currentItem = item;
+
+    // hide another items when current item was clicked and the bg-img was be shown
+    allItems.forEach(function (otheritem) {
+      if (otheritem !== currentItem) {
+        otheritem.classList.toggle('hidden');
+      }
+      else if (otheritem === currentItem) {
+        otheritem.style.zIndex = '2';
+      }
+    })
+
+    // Text block will be different from img and iframe, need to handle separately,cuz I need to change the .bg-img's color and background color and font size
+    var bgImg = document.querySelector('.bg-img');
+    if (item.tagName === 'IMG') {
+      bgImg.innerHTML = '<img src="' + item.src + '" alt="">';
+    }
+    else if (item.classList.contains('iframe-wrapper')) {
+      var iframeSrc = item.querySelector('iframe').src;
+      bgImg.innerHTML = '<iframe src="' + iframeSrc + '" frameborder="0"></iframe>';
+    }
+    else if (item.tagName === 'P') {
+      bgImg.innerHTML = '<p>' + item.textContent + '</p>';
+      bgImg.classList.toggle('is-active-text');
+    }
+    bgImg.classList.toggle('is-active');
+  });
+
+})
+
+
+
+
